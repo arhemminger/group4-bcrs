80ea499191c4e5bc7a4aa2b663eb21c94e17edf8
Andrew Hemminger <arhemminger@gmail.com>
2019-10-21 21:55:27 -0500

    Adding mongoDB connection

--- server/app.js
+++ server/app.js
@@ -30,6 +30,13 @@ const serverPort = 3000;
 
 /************************* Mongoose connection strings go below this line  ***************/
 
+// MongoDB (Atlas) connection string
+const connString = 'mongodb+srv://admin:bu12345@bcrs-waeqk.mongodb.net/bcrs?retryWrites=true&w=majority'
+
+// MongoDB connect
+mongoose.connect(connString, {promiseLibrary: require('bluebird'), useNewUrlParser: true})
+        .then(() => console.debug('Connection to the MongoDB instance was successful'))
+        .catch((err) => console.debug('MongoDB Error: ' + err.message));
 
 /************************* API routes go below this line ********************/
 
