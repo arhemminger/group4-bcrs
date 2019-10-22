c4f9c2a11331d6b928d6c815779d816ee557023c
wthomason <williamthomason92@yahoo.com>
2019-10-21 20:38:48 -0500

    created application commponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,19 @@ import { HomeComponent } from './pages/home/home.component';
 import { SessionLayoutComponent } from './shared';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { Status500Component } from './pages/status500/status500.component';
+import { GraphComponent } from './pages/graph/graph.component';
+import { UsersComponent } from './pages/users/users.component';
+import { SecurityQuestionsComponent } from './pages/security-questions/security-questions.component';
+import { AuthGuardComponent } from './shared/auth-guard/auth-guard.component';
+import { UserEditComponent } from './shared/user-edit/user-edit.component';
+import { ForgotPasswordComponent } from './shared/forgot-password/forgot-password.component';
+import { InvoiceSummaryComponent } from './shared/invoice-summary/invoice-summary.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +31,20 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     HomeComponent,
     SessionLayoutComponent,
     LoginComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    RegisterComponent,
+    AboutComponent,
+    ContactComponent,
+    ShopComponent,
+    ProfileComponent,
+    Status500Component,
+    GraphComponent,
+    UsersComponent,
+    SecurityQuestionsComponent,
+    AuthGuardComponent,
+    UserEditComponent,
+    ForgotPasswordComponent,
+    InvoiceSummaryComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/status500/status500.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status500',
+  templateUrl: './status500.component.html',
+  styleUrls: ['./status500.component.css']
+})
+export class Status500Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
